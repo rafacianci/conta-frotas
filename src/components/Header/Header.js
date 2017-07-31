7b6859bbe65d2c3b78cc83e0d3e0a2b3bc28f199
Rafa Cianci <rafael.fuzifaru@gmail.com>
2017-07-31 15:57:20 -0300

    create the header element

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const header = () => (
+    <header>
+        <h1 className='icon logo'><span className='hidden'>Conta Azul - Tropas</span></h1>
+    </header>
+);
+
+export default header;
